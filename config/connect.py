100d19ee3618b716fb09b168d171b7050957622e
Shai Rainer <shaira@std.hac.ac.il>
2024-10-10 13:46:22 +0300

    add connection

--- config/connect.py
+++ config/connect.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017')
 chicago_car_accidents_db = client['chicago_car_accidents']
 
-
+accidents = chicago_car_accidents_db['accidents']
 daily = chicago_car_accidents_db['days']
 weekly = chicago_car_accidents_db['weeks']
 monthly = chicago_car_accidents_db['months']
